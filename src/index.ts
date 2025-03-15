220bccba12ccd4d90c838b61ed4a6078be8ac507
Roushan <kumar7roushanjha2005@gmail.com>
2025-03-16 02:09:51 +0530

    add JWT authentication: implement login route and AuthController

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import router from './routes';
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (_, res) => {
   res.send('Hello World!');
 });
-
+// Routes 
+app.get('/api',router);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 }); 
---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import AuthController from "../controllers/AuthController";
+
+const router = Router();
+
+// Auth Routes
+router.post("/auth/login", AuthController.login);
+
+
+
+export default router;
